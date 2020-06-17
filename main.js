274617ee2ad09f070750a3163ca1fc40da692127
shumpei14 <pein14soccer@gmail>
2020-06-18 00:25:05 +0900

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-main.js
+'use strict';
+
+const score = 40;
+
+// if (score >=80) {
+// 	console.log('Greate!');
+// } else if (score >=60) {
+// 	console.log('good');
+// }else{
+// 	console.log('ok..');
+// }
+
+
+score >= 80 ? console.log('Great!') :	console.log('ok..');
