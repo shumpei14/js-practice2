f0ddc85a834fc18a653d9812deb486ca1831179e
shumpei14 <pein14soccer@gmail>
2020-06-18 11:39:28 +0900

    練習

--- main.js
+++ main.js
@@ -1,14 +1,55 @@
 'use strict';
 
-const score = 40;
+// const score = 85;
 
-// if (score >=80) {
-// 	console.log('Greate!');
-// } else if (score >=60) {
-// 	console.log('good');
-// }else{
-// 	console.log('ok..');
+// if (score >= 80) {
+//   console.log('Great!');
+// } else {
+//   console.log('OK...!');
+// }
+
+// score >= 80 ? console.log('Great!') : console.log('OK...!');
+
+// const score = 60;
+// const name ='taguch'
+
+// if (score >= 50) {
+// 	if (name === 'taguch') {
+// 		console.log('Good job');
+// 	}
+
+
+// &&なおかつ(AND)
+// ||もしくは(or)
+// !〜ではない(not)
+
+// if (score >= 50 && name ==='taguch') {
+// 	console.log('good job');
 // }
 
 
-score >= 80 ? console.log('Great!') :	console.log('ok..');
+const signal = 'pink';
+
+// if (signal === 'red') {
+// 	console.log('stop');
+// } else if (signal === 'yellow') {
+// 	console.log('coution');
+// } else if (signal === 'blue') {
+// 	console.log('go');
+// }
+
+
+switch (signal) {
+case 'red':
+	console.log('stop');
+	break;
+case 'yellow':
+	console.log('coution');
+	break;
+case 'blue':
+	console.log('go');
+	break;
+default:
+console.log('wrong signal');
+break;
+}
