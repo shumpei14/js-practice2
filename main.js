55318c84f613f279cb148a761f7d51b84dda433b
shumpei14 <pein14soccer@gmail>
2020-06-19 12:04:02 +0900

    jsjs

--- main.js
+++ main.js
@@ -82,19 +82,19 @@
 // }
 
 
-function showAd() {
-  console.log('----------');
-  console.log('--- Ad ---');
-  console.log('----------');
-}
+// function showAd() {
+//   console.log('----------');
+//   console.log('--- Ad ---');
+//   console.log('----------');
+// }
 
-showAd();
-console.log('Tom is great!');
-console.log('Bob is great!');
-showAd();
-console.log('Steve is great!');
-console.log('Richard is great!');
-showAd();
+// showAd();
+// console.log('Tom is great!');
+// console.log('Bob is great!');
+// showAd();
+// console.log('Steve is great!');
+// console.log('Richard is great!');
+// showAd();
 
 
 
