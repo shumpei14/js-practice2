d9cc76493d13638d6bc573f9a8fafeb11b559980
shumpei14 <pein14soccer@gmail>
2020-06-21 10:05:11 +0900

    jsp

--- main.js
+++ main.js
@@ -1,110 +1,115 @@
 'use strict';
 
-// const score = 85;
+const score = 85;
 
-// if (score >= 80) {
-//   console.log('Great!');
-// } else {
-//   console.log('OK...!');
-// }
+if (score >= 80) {
+  console.log('Great!');
+} else {
+  console.log('OK...!');
+}
 
-// score >= 80 ? console.log('Great!') : console.log('OK...!');
+score >= 80 ? console.log('Great!') : console.log('OK...!');
 
-// const score = 60;
-// const name ='taguch'
+const score = 60;
+const name ='taguch'
 
-// if (score >= 50) {
-// 	if (name === 'taguch') {
-// 		console.log('Good job');
-// 	}
-
-
-// &&なおかつ(AND)
-// ||もしくは(or)
-// !〜ではない(not)
-
-// if (score >= 50 && name ==='taguch') {
-// 	console.log('good job');
-// }
+if (score >= 50) {
+	if (name === 'taguch') {
+		console.log('Good job');
+	}
 
 
-// const signal = 'pink';
+&&なおかつ(AND)
+||もしくは(or)
+!〜ではない(not)
 
-// if (signal === 'red') {
-// 	console.log('stop');
-// } else if (signal === 'yellow') {
-// 	console.log('coution');
-// } else if (signal === 'blue') {
-// 	console.log('go');
-// }
+if (score >= 50 && name ==='taguch') {
+	console.log('good job');
+}
 
 
-// switch (signal) {
-// case 'red':
-// 	console.log('stop');
-// 	break;
-// case 'yellow':
-// 	console.log('coution');
-// 	break;
-// case 'blue':
-// 	console.log('go');
-// 	break;
-// default:
-// console.log('wrong signal');
-// break;
-// }
+const signal = 'pink';
 
-// for (let i = 1; i <= 10; i++) {
-// 	console.log(`hello ${i}`)
-// }
+if (signal === 'red') {
+	console.log('stop');
+} else if (signal === 'yellow') {
+	console.log('coution');
+} else if (signal === 'blue') {
+	console.log('go');
+}
 
 
+switch (signal) {
+case 'red':
+	console.log('stop');
+	break;
+case 'yellow':
+	console.log('coution');
+	break;
+case 'blue':
+	console.log('go');
+	break;
+default:
+console.log('wrong signal');
+break;
+}
 
-// let hp = 100;
-
-// while (hp > 0) {
-// 	console.log(`${hp} HP left`);
-// 	hp -= 15;
-// }
-
-
-// 
-
-// for (let i = 1; i <= 10; i++) {
-// 	// 
-// 	// if (i % 3 === 0){
-// 	// 	continue;
-// 	// }
-// 	if (i === 4){
-// 		break;
-// 	}
-// 	console.log(i);
-// }
+for (let i = 1; i <= 10; i++) {
+	console.log(`hello ${i}`)
+}
 
 
-// function showAd() {
-//   console.log('----------');
-//   console.log('--- Ad ---');
-//   console.log('----------');
-// }
 
-// showAd();
-// console.log('Tom is great!');
-// console.log('Bob is great!');
-// showAd();
-// console.log('Steve is great!');
-// console.log('Richard is great!');
-// showAd();
+let hp = 100;
+
+while (hp > 0) {
+	console.log(`${hp} HP left`);
+	hp -= 15;
+}
 
 
 
 
+for (let i = 1; i <= 10; i++) {
+	// 
+	// if (i % 3 === 0){
+	// 	continue;
+	// }
+	if (i === 4){
+		break;
+	}
+	console.log(i);
+}
+
+
+function showAd(message = 'ad') {
+  console.log('----------');
+  console.log(`--- ${message} ---`);
+  console.log('----------');
+}
+
+showAd('Header Ad');
+console.log('Tom is great!');
+console.log('Bob is great!');
+showAd();
+console.log('Steve is great!');
+console.log('Richard is great!');
+showAd('Footer Ad');
 
 
 
 
+function sum(a, b, c) {
+  // console.log(a + b + c);
+  return a + b + c;
+  console.log(a + b + c);
+}
 
+// sum(1, 2, 3);
+// sum(3, 4, 5);
 
+const total = sum(1, 2, 3) + sum(3, 4, 5); // 18
+// console.log(total);
 
 
 
